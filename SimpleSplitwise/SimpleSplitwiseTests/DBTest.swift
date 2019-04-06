414241d559793c1fa0be4c5b839a9c5a4d4f3709
Bon <anhbontb@gmail.com>
2019-04-06 18:05:55 +0700

    [Test] add test for friend table

---
+++ SimpleSplitwise/SimpleSplitwiseTests/DBTest.swift
@@ -0,0 +1,43 @@
+//
+//  DBTest.swift
+//  SimpleSplitwiseTests
+//
+//  Created by Bon Bon on 4/6/19.
+//  Copyright © 2019 VNG. All rights reserved.
+//
+
+import XCTest
+import RealmSwift
+
+@testable import SimpleSplitwise
+
+class TestFriendTable: SWFriendTableProtocol {}
+
+class DBTest: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    func testFriendTable() {
+        let nameA = "hello"
+        let nameB = "world"
+        let friendA = SWFriend.init(name: nameA)
+        let friendB = SWFriend.init(name: nameB)
+        
+        let db = TestFriendTable()
+        db.add(friend: friendA)
+        db.add(friend: friendB)
+        
+        let allFriend: [SWFriend]? = db.getAllFriend()
+        XCTAssert(allFriend?.count == 2, "add friend fail")
+        
+        let firt = allFriend?.first
+        let last = allFriend?.last
+        
+        XCTAssert(firt?.name == nameA, "data error 2")
+        XCTAssert(last?.name == nameB, "data error 2")
+        
+    }
+
+}
