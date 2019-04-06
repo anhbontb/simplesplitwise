231ceb2c836e3db1221b9b56d3c7a62f7d1a9416
Bon <anhbontb@gmail.com>
2019-04-06 14:52:45 +0700

    [Project] init project

---
+++ SimpleSplitwise/SimpleSplitwise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleSplitwise
+//
+//  Created by Bon Bon on 4/6/19.
+//  Copyright © 2019 VNG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
